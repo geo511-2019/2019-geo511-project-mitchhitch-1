86ba281e0d52a1363c7ae20ec3c63f1ab8b3638a
mitchhitch <54652566+mitchhitch@users.noreply.github.com>
2019-12-02 23:46:32 -0500

    Organized major funcitons to be used in shiny app
    Still need to get reactive shiny app working

---
+++ prjkt/FuncsBestSites.R
@@ -0,0 +1,93 @@
+
+
+###Function to unify csv's to sf objects with same crs as File1 
+#####(note that File 1 must be a shape file)
+#####This should be run AFTER columns have been trimmed
+TransCoord_csv<-function(File1,FileX,FileX_Lon="Longitude",FileX_Lat="Latitude",FileX_crs=4326){
+  
+  FileX<-st_as_sf(FileX,coords=c(FileX_Lon,FileX_Lat)) %>% 
+    st_set_crs(FileX_crs) %>% 
+    st_transform(st_crs(File1)) %>% 
+    st_crop(st_bbox(File1))
+  FileX
+  
+}
+
+
+
+### Join all files into 1 sf ONLY run afer TransCoord_csv or st_transform for shape files.
+All_F<-function(File1,...){
+  OtherF<-list(...)
+  countF<-length(OtherF)
+  MainF<-File1
+  for(i in 1:countF){
+    MainF=st_join(OtherF[[1]],MainF,join=st_nn,k=1,maxdist=500) %>% 
+      aggregate( by=list(.[,1]),FUN=unique,do_union=TRUE,join = st_intersects)
+    
+  }
+
+}
+
+
+
+
+###This ranks the similarit of rows
+score.calc<-function(rowprime,rowtest){
+  
+  colscore<-vector(length=length(rowprime))
+  for(i in 1:ncol(rowprime)){
+    prime<-rowprime[i]
+    if(is.list(prime)){
+      prime<-unlist(prime)
+    }
+    test<-rowtest[i]
+    if(is.list(test)){
+      test<-unlist(test)
+    }
+    if(length(prime)>1||length(test)>1){
+      colscore[i]<-as.double(length(c(
+        setdiff(prime,test),
+        setdiff(test,prime)))
+      )
+      
+    }
+    else if(is.double(prime)||is.numeric(prime)){
+      colscore[i]<-as.double(abs((prime-test)/(prime+test)))
+      
+    }
+    else if(is.factor(prime)||is.character(prime)){
+      if(prime==test){
+        colscore[i]<-0
+        
+      }
+      else{
+        colscore[i]<-1
+        
+      }
+    }
+    else 
+      colscore[i]<-NA
+  }
+  
+  return(sum(colscore))
+}
+
+
+###Select best sets of sites
+Out_best<-function(possible_n){
+  #Determine min possible value for sets
+minimum<-foreach(i=1:length(possible_n),.combine=c)%dopar%{
+  sum(possible_n[[i]]$Score)
+} %>% 
+  min()
+  #Select sets with min possible value
+best<-foreach(i=1:length(possible_n))%dopar%{
+  if(sum(possible_n[[i]]$Score)==minimum){
+    return(possible_n[[i]])
+  }
+} %>% 
+  compact()
+
+best
+
+}
--- prjkt/app.R
+++ prjkt/app.R
@@ -13,92 +13,38 @@ library(datasets)
 library(miniUI)
 
 ui <- fluidPage(
-    
-    sidebarLayout(
-        sidebarPanel( 
-            ####user uploads points of interest####
-            fileInput("file1", "Upload point data (csv only)",
-                      # limit the file extention to the following:
-                      accept=c("text/csv", 
-                               "text/comma-separated-values,text/plain", 
-                               ".csv")),
-        
-            ###Option to use default(erie county parcels)
-            selectInput("shapselect", label = "Use default(only Erie Count, NY) shape file?",
-                        choices = c(TRUE,FALSE), selected = FALSE),
-            
-         #CHANGE:This part should be in the reactive server#############
-            # if(shapeselect=FALSE)
-            #     fileInput("file2", "Choose optional shape File",accept=".shp"),
-            
-            h5("Upload file for data analysis"),
-            
-            # no choices before a file uploaded
-            uiOutput("columns"),
-            
-            # inputs to generate pivot table (demo in a single tab here, but in real app, this will be on a different tab)
-            selectInput("vars", "vars to use:", choices = NULL, multiple = TRUE, selected = NULL),
-            selectInput("fromvars", "select from vars:", choices = NULL, multiple = TRUE, selected = NULL)
-        ),
-        mainPanel( 
-            verbatimTextOutput("print")
-        ))
+    titlePanel("Old Faithful Geyser Data"),
+    mainPanel(
+        actionButton("updater", "Update message"),
+        textOutput("textbox")
+    )
 )
 
-server <- function(input, output, session) {
-    options(shiny.maxRequestSize=30*1024^2)
-    dt <- reactiveValues()
-    
-    # upload file
-    observeEvent(input$file, {
-        inFile <- input$file
-        req(inFile)
-        # upload the file to dataset
-        dt$data = read.csv(inFile$datapath, header = TRUE)
-    })
-    
-    # create columns groupcheckbox ui
-    output$columns <- renderUI({
-        
-        # get the col names of f and assign them to a list
-        cols = mapply(list, names(dt$data))
-        
-        # render column group checkbox ui after loading the data
-        checkboxGroupInput("columns", "Select columns to display", choices = cols, selected = cols)
-    })
 
-    ######## dependent on columns selection ########
-    observeEvent(input$columns, {
-        dt$cols <- input$columns
-        if (is.null(dt$cols)) dt$cols <- ""
-        # update input$vars for pivottable tab
-        print("observerEvent")
-        print(dt$cols)
-        
-        updateSelectInput(session, "vars", "vars to use:", choices = dt$cols, selected = NULL)
-    }, ignoreNULL = FALSE)
-    
-    
-    observeEvent(input$vars, {
-        if( is.null(input$vars)) { 
-            updateSelectInput(session, "fromvars", "select from vars:", choices = "", selected = NULL)  
-        }
-        else {
-            updateSelectInput(session, "fromvars", "select from vars:", choices = input$vars, selected = NULL)
-        }
-    }, ignoreNULL = FALSE)
-    
-    output$print <- renderPrint(
-        list(
-            paste("input$columns:", paste(input$columns,collapse=",")),
-            paste("input$vars:",paste(input$vars,collapse=",")), 
-            paste("input$fromvars:", paste(input$fromvars,collapse=","))
-        )
+glycoPipe <- function() {
+    start_time <- Sys.time()
+    list(
+        today      = weekdays(start_time, abbreviate = FALSE),
+        start_time = start_time
     )
 }
+
+
+server <- function(input, output) {
+    result <- glycoPipe()
+    start_time_text <- strftime(result$start_time, "%H:%M:%S")
+    
+    output$textbox <- renderText({
+        input$updater
+        elapsed <- Sys.time() - result$start_time
+        sprintf(
+            "Started at %s on %s, which was %.0f seconds ago",
+            start_time_text,
+            result$today,
+            elapsed
+        )
+    })
+}
+
+
 shinyApp(ui = ui, server = server)
-
-
-
-
-
